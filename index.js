80803971f41079a54b19f2a45c72b035c9b01493
edogola4 <edogola4@gmail.com>
2024-10-20 22:58:24 +0300

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+// findMatching
+function findMatching(drivers , name){
+return drivers.filter(driver => driver.toLowerCase () === name.toLowerCase());
+}
+
+
+// fuzzyMatch()
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+
+// MatchName
+function matchName(drivers, name){
+    return drivers.filter(drivers => drivers.name === name);
+}
